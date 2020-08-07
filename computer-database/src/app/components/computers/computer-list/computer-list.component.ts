fed7f1893147ab977976f24f7aa5a70b32dee0aa
kylpowtake <kylianviaux@hotmail.fr>
2020-08-07 17:23:20 +0200

    Implementing computersList logic and removeComputer logic

--- computer-database/src/app/components/computers/computer-list/computer-list.component.ts
+++ computer-database/src/app/components/computers/computer-list/computer-list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Computer } from '../../../Models/computer.model';
+import { ActivatedRoute } from '@angular/router';
+import { ComputerService } from 'src/app/service/computer.service';
+import { HttpResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-computer-list',
@@ -6,10 +10,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./computer-list.component.scss']
 })
 export class ComputerListComponent implements OnInit {
+  private route : ActivatedRoute;
+  computersList : Computer[];
+  computerService : ComputerService;
 
-  constructor() { }
+  constructor(private routeParam: ActivatedRoute, computerService: ComputerService) {
+    this.route = routeParam;
+    this.computerService = computerService;
+   }
 
   ngOnInit(): void {
+    this.getList
   }
 
+  getList(){
+    this.computerService.getComputers().suscribe()(
+      (result: Computer[]) => {
+        this.computersList = result;
+      },
+      (error: any) => {
+        console.log("Erreur avec l'observable lors du getComputersList.");
+      }
+    );
+  }
+
+  remove(id : number): void{
+    this.computerService.deleteComputer(id).subscribe(
+      (result: HttpResponse<string>) => {
+        const message = result.body;
+        const index = this.computersList.findIndex(x => x.id == id);
+        if(index != -1){
+          this.computersList.splice(index, 1);
+        }
+      },
+      (error : HttpResponse<String>) => {
+        console.log("Erreur avec l'observable lors du getComputersList." + error.status);
+      }
+    );
+  }
 }
