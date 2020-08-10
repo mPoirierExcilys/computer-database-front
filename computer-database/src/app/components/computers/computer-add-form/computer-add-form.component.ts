d31c998caf690354c6056932584ec1a9e57f5051
Paul-Augustin Duchene <paul-augustin_duchene@orange.fr>
2020-08-10 12:19:12 +0200

    continue formular commit to merge services and rooting

--- computer-database/src/app/components/computers/computer-add-form/computer-add-form.component.ts
+++ computer-database/src/app/components/computers/computer-add-form/computer-add-form.component.ts
@@ -1,21 +1,40 @@
+import { CustomMaterialModule } from './../../../custom-material/custom-material.module';
+import { Company } from './../../../Models/company.model';
 import { ComputerService } from './../../../service/computer.service';
 import { Computer } from './../../../Models/computer.model';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-computer-add-form',
   templateUrl: './computer-add-form.component.html',
   styleUrls: ['./computer-add-form.component.scss']
 })
+
+
+
 export class ComputerAddFormComponent implements OnInit {
 
 
   computer: Computer = new Computer();
+  companies: Company[];
 
-  constructor(private computerservice: ComputerService) { }
+  constructor(private computerService: ComputerService, private companyService: CompanyService) { }
+
+  onSubmit(){
+    this.computerService.createComputer(this.computer).subscribe();
+    console.log(this.computer.name);
+  }
 
 
   ngOnInit(): void {
+    this.companyService.getCompanies().subscribe(
+      (result: Company[]) => {
+          this.companies = result; 
+      },
+      (error) => {
+          console.log("can't retrive company list")
+      }
+  );
   }
 
 }
