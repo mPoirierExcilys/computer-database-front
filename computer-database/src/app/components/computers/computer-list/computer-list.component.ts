3f22de7bf0668a24868d57437773d5a6402cb79c
kylpowtake <kylianviaux@hotmail.fr>
2020-08-10 14:14:16 +0200

    Modification of list following the add of routing and service

--- computer-database/src/app/components/computers/computer-list/computer-list.component.ts
+++ computer-database/src/app/components/computers/computer-list/computer-list.component.ts
@@ -1,8 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { Computer } from '../../../Models/computer.model';
+import { Page } from '../../../Models/page.model';
 import { ActivatedRoute } from '@angular/router';
 import { ComputerService } from 'src/app/service/computer.service';
 import { HttpResponse } from '@angular/common/http';
+import { ValueConverter } from '@angular/compiler/src/render3/view/template';
+
+
+interface Order {
+  label : string;
+  name : string;
+}
 
 @Component({
   selector: 'app-computer-list',
@@ -13,6 +21,16 @@ export class ComputerListComponent implements OnInit {
   private route : ActivatedRoute;
   computersList : Computer[];
   computerService : ComputerService;
+  page : Page;
+  orders: Order[] = [
+    {label: "id", name: "computer.id"},
+    {label: "name", name: "computer.name"},
+    {label: "introduced", name: "computer.introduced"},
+    {label: "discontinued", name: "computer.discontinued"},
+    {label: "company name", name: "cp.name"}
+
+  ]
+
 
   constructor(private routeParam: ActivatedRoute, computerService: ComputerService) {
     this.route = routeParam;
@@ -20,12 +38,16 @@ export class ComputerListComponent implements OnInit {
    }
 
   ngOnInit(): void {
-    this.getList
+    console.log("Dans init.");
+    this.getList();
   }
 
   getList(){
-    this.computerService.getComputers().suscribe()(
+    this.setPage("ASC", 1, 25, "id");
+    console.log("Dans list.");
+    this.computerService.getComputers(this.page, "").subscribe(
       (result: Computer[]) => {
+        console.log("Dans bien.");
         this.computersList = result;
       },
       (error: any) => {
@@ -35,17 +57,14 @@ export class ComputerListComponent implements OnInit {
   }
 
   remove(id : number): void{
-    this.computerService.deleteComputer(id).subscribe(
-      (result: HttpResponse<string>) => {
-        const message = result.body;
-        const index = this.computersList.findIndex(x => x.id == id);
-        if(index != -1){
-          this.computersList.splice(index, 1);
-        }
-      },
-      (error : HttpResponse<String>) => {
-        console.log("Erreur avec l'observable lors du getComputersList." + error.status);
-      }
-    );
+    this.computerService.deleteComputer(id).subscribe();
+  }
+
+  setPage(ascending: string, currentPage: number, itemsByPage: number, order: string){
+    this.page = new Page();
+    this.page.ascending = ascending;
+    this.page.currentPage = currentPage;
+    this.page.itemsByPage = itemsByPage;
+    this.page.order = order;
   }
 }
