2abba9578959f8ef47e8906a559962572a5acc60
Paul-Augustin Duchene <paul-augustin_duchene@orange.fr>
2020-08-11 11:28:37 +0200

    adding redirection to homepage

--- computer-database/src/app/components/computers/computer-add-form/computer-add-form.component.ts
+++ computer-database/src/app/components/computers/computer-add-form/computer-add-form.component.ts
@@ -5,7 +5,7 @@ import { ComputerService } from './../../../service/computer.service';
 import { Computer } from './../../../Models/computer.model';
 import { Component, OnInit} from '@angular/core';
 import { Location } from '@angular/common';
-
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -22,11 +22,17 @@ export class ComputerAddFormComponent implements OnInit {
   computer: Computer = new Computer();
   companies: Company[];
 
-  constructor(private computerService: ComputerService, private companyService: CompanyService, private location: Location) { }
+  constructor(private computerService: ComputerService, private companyService: CompanyService, private location: Location, private router: Router) { }
 
   onSubmit(){
     this.computerService.createComputer(this.computer).subscribe();
+    this.returnHome();
     console.log(this.computer.name);
+  }
+
+  returnHome(){
+    this.router.navigate(['computers']);
+    console.log("going home");
   }
 
   onCancel(){
