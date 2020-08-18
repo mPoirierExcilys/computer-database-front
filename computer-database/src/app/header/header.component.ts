480dc5d342eb6f51848dc69f6684125fe7cc072c
Paul-Augustin Duchene <paul-augustin_duchene@orange.fr>
2020-08-18 17:56:52 +0200

    try to get user.roles

--- computer-database/src/app/header/header.component.ts
+++ computer-database/src/app/header/header.component.ts
@@ -1,9 +1,10 @@
+import { User } from './../Models/user.model';
+import { map } from 'rxjs/operators';
 import { Role } from './../Models/role.model';
 import { Observable } from 'rxjs';
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
-import {MatDialog} from '@angular/material/dialog';
+import { MatDialog } from '@angular/material/dialog';
 import { ComputerAddFormComponent } from '../components/computers/computer-add-form/computer-add-form.component';
-import { User } from '../Models/user.model';
 import { UserService } from '../service/user.service';
 import { Router } from '@angular/router';
 import { UserLoginComponent } from '../components/users/user-login/user-login.component'
@@ -17,13 +18,13 @@ import { UserLoginComponent } from '../components/users/user-login/user-login.co
 export class HeaderComponent implements OnInit {
 
   user: User;
-  userRoles: Role[];
+  userForRole: User;
 
   messageLogout = "Logout";
 
   @Output() logoutEvent = new EventEmitter<string>();
-  
-  constructor(public dialog: MatDialog, private router: Router, private userService: UserService) {}
+
+  constructor(public dialog: MatDialog, private router: Router, private userService: UserService) { }
 
   openDialog() {
     const dialogRef = this.dialog.open(ComputerAddFormComponent);
@@ -33,20 +34,33 @@ export class HeaderComponent implements OnInit {
     });
   }
 
-
-  getUserRole(){
+  getUserRoles() {
     this.userService.getUser().subscribe((result: User) => {
-      this.user = result;
-    })
+      console.log("result : " + result.roles[0].name)
+      console.log("result : " + result.roles[1].name)
+      this.user. roles = result.roles;
+    }, (error) => { console.log(error);
+  });
+}
 
-  }
+setUser(){
+  this.user = this.userService.currentUserValue;
+}
+
+showUser(){
+  
+}
 
 
-  ngOnInit(): void {
-    this.getUserRole();
-  }
+ngOnInit(): void {
+  this.setUser();
+  this.getUserRoles();
+  console.log(this.user.username);
+  console.log(this.user.token);
+  console.log(this.user.roles);
+}
 
-  sendLogout() {
-    this.router.navigate(['/login'], { state : { isToLogout : true}});
-  }
+sendLogout() {
+  this.router.navigate(['/login'], { state: { isToLogout: true } });
+}
 }
