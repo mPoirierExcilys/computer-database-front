dc08845541801c5c1e1e5ed9f61f197d8b5424c7
soniakr <sonia.koraichi@hotmail.com>
2020-08-21 12:29:39 +0200

    footer + list users

--- computer-database/src/app/header/header.component.ts
+++ computer-database/src/app/header/header.component.ts
@@ -80,7 +80,7 @@ export class HeaderComponent implements OnInit {
 
   openDialogUser(): void {
     const dialogRef = this.dialog.open(UserAddFormComponent, {
-      width: '720px',
+      width: '750px',
     });
 
     dialogRef.afterClosed().subscribe(result => {
